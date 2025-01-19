ed3230f4fd85de6b55ccade9bf1e0d2e2a45c2ee
James Smith <james@smithjw.me>
2025-01-19 01:00:45 +0000

    Test cache for go

---
+++ tools.go
@@ -0,0 +1,14 @@
+package tools
+
+import (
+	_ "github.com/MakeNowJust/heredoc"
+	_ "github.com/docker/cli"
+	_ "github.com/docker/docker"
+	_ "github.com/goware/prefixer"
+	_ "github.com/hexops/gotextdiff"
+	_ "github.com/moby/moby"
+	_ "github.com/moby/sys/signal"
+	_ "github.com/spf13/cobra"
+	_ "gopkg.in/yaml.v3"
+	_ "rsc.io/script"
+)
